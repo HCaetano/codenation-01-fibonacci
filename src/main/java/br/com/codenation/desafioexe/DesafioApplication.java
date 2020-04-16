f53260a3458906c6123cb896bf56bf6ead75c83b
Havyner Caetano <hrcgeo@gmail.com>
2020-04-16 18:51:34 -0300

    Implementa funcionalidades solicitadas

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -1,15 +1,31 @@
 package br.com.codenation.desafioexe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DesafioApplication {
 
 	public static List<Integer> fibonacci() {
-		return null;
+		int numeroLimite = 350;
+		int numeroAnterior = 0;
+		int proximoNumero = 1;
+		List<Integer> sequenciaFibonacci = new ArrayList<>();
+
+		while (numeroAnterior <= numeroLimite) {
+			sequenciaFibonacci.add(numeroAnterior);
+
+			int soma = numeroAnterior + proximoNumero;
+			numeroAnterior = proximoNumero;
+			proximoNumero = soma;
+		}
+		sequenciaFibonacci.add(numeroAnterior);
+
+		return sequenciaFibonacci;
 	}
 
 	public static Boolean isFibonacci(Integer a) {
-		return null;
+
+		return fibonacci().contains(a);
 	}
 
 }
